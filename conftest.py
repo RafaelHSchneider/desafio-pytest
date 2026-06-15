37fe1e205c986ef7e33526eb3117088319d02ea4
RafaelHSchneider <rafaelhenriquesch@gmail.com>
2026-06-15 14:46:07 -0300

    test:add tests for POST /users endpoint

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+import uuid
 
 BASE_URL = "https://compassuol.serverest.dev"
 
@@ -7,3 +8,46 @@ BASE_URL = "https://compassuol.serverest.dev"
 def usuarios_response():
     return requests.get(f"{BASE_URL}/usuarios")
 
+@pytest.fixture
+def usuario_payload():
+    return {
+        "nome": "Fulano da Silva",
+        "email": f"teste_{uuid.uuid4().hex[:8]}@qa.com.br",
+        "password": "teste",
+        "administrador": "true"
+    }
+
+
+@pytest.fixture
+def usuario_cadastrado(usuario_payload):
+    response = requests.post(
+        f"{BASE_URL}/usuarios",
+        json=usuario_payload
+    )
+
+    body = response.json()
+
+    yield response
+
+    # Teardown
+    if response.status_code == 201:
+        usuario_id = body["_id"]
+
+        requests.delete(
+            f"{BASE_URL}/usuarios/{usuario_id}"
+        )
+
+@pytest.fixture
+def usuario_existente(usuario_payload):
+    response = requests.post(
+        f"{BASE_URL}/usuarios",
+        json=usuario_payload
+    )
+
+    usuario = response.json()
+
+    yield usuario, usuario_payload
+
+    requests.delete(
+        f"{BASE_URL}/usuarios/{usuario['_id']}"
+    )
--- tests/test_usuarios.py
+++ tests/test_usuarios.py
@@ -1,5 +1,8 @@
 import requests
 
+
+BASE_URL = "https://compassuol.serverest.dev"
+
 def test_listar_usuarios_status_code(usuarios_response):
     assert usuarios_response.status_code == 200
 
@@ -38,3 +41,43 @@ def test_listar_usuarios_estrutura_usuario(usuarios_response):
 
     for campo in campos_obrigatorios:
         assert campo in usuario
+
+def test_usuario_campos_possuem_tipo_correto(usuarios_response):
+    usuario = usuarios_response.json()["usuarios"][0]
+
+    assert isinstance(usuario["nome"], str)
+    assert isinstance(usuario["email"], str)
+    assert isinstance(usuario["password"], str)
+    assert isinstance(usuario["administrador"], str)
+    assert isinstance(usuario["_id"], str)
+
+def test_cadastrar_usuario_com_sucesso(usuario_cadastrado):
+    assert usuario_cadastrado.status_code == 201
+
+    body = usuario_cadastrado.json()
+
+    assert body["message"] == "Cadastro realizado com sucesso"
+    assert "_id" in body
+
+def test_cadastrar_usuario_retorna_campos_esperados(usuario_cadastrado):
+    body = usuario_cadastrado.json()
+
+    assert "message" in body
+    assert "_id" in body
+
+    assert len(body.keys()) == 2
+
+def test_cadastrar_usuario_retorna_json(usuario_cadastrado):
+    assert "application/json" in usuario_cadastrado.headers["Content-Type"]
+
+
+def test_cadastrar_usuario_com_email_ja_existente(usuario_existente):
+    _, payload = usuario_existente
+
+    response = requests.post(
+        f"{BASE_URL}/usuarios",
+        json=payload
+    )
+
+    assert response.status_code == 400
+    assert response.json()["message"] == "Este email já está sendo usado"
